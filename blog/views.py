d426514ee72c8fd007e957d04062a23dafb32dd8
Somye <anyone.mahajan@gmail.com>
2025-06-05 16:59:34 +0530

    Added DTL post loading

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from blog.models import Post
+
+
 # Create your views here.
 def post_list(req):
-    return render(req, 'blog/post_list.html')
+    posts = Post.objects.order_by('-published_at')
+    return render(req, 'blog/post_list.html', {'posts' : posts})
